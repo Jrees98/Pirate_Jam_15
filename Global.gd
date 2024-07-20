5da0838e6a2561536891017d9a20152cf8670247
Jacob Rees <reescodes@gmail.com>
2024-07-20 13:06:05 -0600

    add shop UI and backpack1

--- Global.gd
+++ Global.gd
@@ -9,6 +9,8 @@ var coal_chance
 var gem_price : int
 var coal_price : int
 
+var personal_inventory_size : int
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	blue_gem_chance = 5
@@ -16,6 +18,8 @@ func _ready():
 	
 	gem_price = 5
 	coal_price = 2
+	
+	personal_inventory_size = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Scenes/shop.gd
+++ Scenes/shop.gd
@@ -1,12 +1,34 @@
 extends Area2D
 
+var backpack1_cost := 50
+
+
+
+func _on_backpack_1_mouse_entered():
+	$CanvasLayer/Panel/Backpack1/ButtonSprite/Border1.show()
+	$CanvasLayer/Panel/Backpack1/Timer.start()
+
+
+func _on_backpack_1_mouse_exited():
+	var border1 = $CanvasLayer/Panel/Backpack1/ButtonSprite/Border1
+	border1.visible = not border1.visible
+	$CanvasLayer/Panel/Backpack1/Timer.stop()
+
+
+func _on_backpack_1_pressed():
+	if  Global.total_coins >= backpack1_cost:
+		Global.personal_inventory_size = 10
+		$CanvasLayer/Panel/Backpack1.queue_free()
+
+
+func _on_timer_timeout():
+	var border1 = $CanvasLayer/Panel/Backpack1/ButtonSprite/Border1
+	border1.visible = not border1.visible
 
 
 func _on_body_entered(body):
-	print("Shop Entered")
-	
-
+	$CanvasLayer.show()
 
 
 func _on_body_exited(body):
-	print("Shop Left")
+	$CanvasLayer.hide()
