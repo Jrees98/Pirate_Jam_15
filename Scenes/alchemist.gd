ceeed8b252f532e40138b6387f63676ea15602d3
Jacob Rees <reescodes@gmail.com>
2024-07-25 21:29:54 -0600

    removed node for organization

--- Scenes/alchemist.gd
+++ Scenes/alchemist.gd
@@ -28,133 +28,133 @@ func _on_body_exited(body):
 ### DIAMOND ###
 
 func _on_button_mouse_entered():
-	$CanvasLayer/Panel/Background/Gems/Diamond/selector.show()
+	$CanvasLayer/Panel/Background/Diamond/selector.show()
 
 
 func _on_button_mouse_exited():
-	$CanvasLayer/Panel/Background/Gems/Diamond/selector.hide()
+	$CanvasLayer/Panel/Background/Diamond/selector.hide()
 
 
 func _on_button_pressed():
 	if all_slots_full:
 		return
 	if slot_1_full == false:
-		$CanvasLayer/Panel/Background/Slots/Slot1/Diamond.show()
-		$CanvasLayer/Panel/Background/Gems/Diamond.hide()
+		$CanvasLayer/Panel/Background/Slot1/Diamond.show()
+		$CanvasLayer/Panel/Background/Diamond.hide()
 		slot_1_full = true
 	elif slot_2_full == false:
-		$CanvasLayer/Panel/Background/Slots/Slot2/Diamond.show()
-		$CanvasLayer/Panel/Background/Gems/Diamond.hide()
+		$CanvasLayer/Panel/Background/Slot2/Diamond.show()
+		$CanvasLayer/Panel/Background/Diamond.hide()
 		slot_2_full = true
 	elif slot_3_full == false:
-		$CanvasLayer/Panel/Background/Slots/Slot3/Diamond.show()
-		$CanvasLayer/Panel/Background/Gems/Diamond.hide()
+		$CanvasLayer/Panel/Background/Slot3/Diamond.show()
+		$CanvasLayer/Panel/Background/Diamond.hide()
 		slot_3_full = true
 	
 
 ### EMERALD ###
 
 func _on_emerald_button_mouse_entered():
-	$CanvasLayer/Panel/Background/Gems/Emerald/selector2.show()
+	$CanvasLayer/Panel/Background/Emerald/selector2.show()
 
 
 func _on_emerald_button_mouse_exited():
-	$CanvasLayer/Panel/Background/Gems/Emerald/selector2.hide()
+	$CanvasLayer/Panel/Background/Emerald/selector2.hide()
 
 
 func _on_emerald_button_pressed():
 	if all_slots_full:
 		return
 	if slot_1_full == false:
-		$CanvasLayer/Panel/Background/Slots/Slot1/Emerald.show()
-		$CanvasLayer/Panel/Background/Gems/Emerald.hide()
+		$CanvasLayer/Panel/Background/Slot1/Emerald.show()
+		$CanvasLayer/Panel/Background/Emerald.hide()
 		slot_1_full = true
 	elif slot_2_full == false:
-		$CanvasLayer/Panel/Background/Slots/Slot2/Emerald.show()
-		$CanvasLayer/Panel/Background/Gems/Emerald.hide()
+		$CanvasLayer/Panel/Background/Slot2/Emerald.show()
+		$CanvasLayer/Panel/Background/Emerald.hide()
 		slot_2_full = true
 	elif slot_3_full == false:
-		$CanvasLayer/Panel/Background/Slots/Slot3/Emerald.show()
-		$CanvasLayer/Panel/Background/Gems/Emerald.hide()
+		$CanvasLayer/Panel/Background/Slot3/Emerald.show()
+		$CanvasLayer/Panel/Background/Emerald.hide()
 		slot_3_full = true
 
 
 ### SAPPHIRE ###
 
 func _on_sapphire_button_mouse_entered():
-	$CanvasLayer/Panel/Background/Gems/Sapphire/selector3.show()
+	$CanvasLayer/Panel/Background/Sapphire/selector3.show()
 
 
 func _on_sapphire_button_mouse_exited():
-	$CanvasLayer/Panel/Background/Gems/Sapphire/selector3.hide()
+	$CanvasLayer/Panel/Background/Sapphire/selector3.hide()
 
 
 func _on_sapphire_button_pressed():
 	if all_slots_full:
 		return
 	if slot_1_full == false:
-		$CanvasLayer/Panel/Background/Slots/Slot1/Sapphire.show()
-		$CanvasLayer/Panel/Background/Gems/Sapphire.hide()
+		$CanvasLayer/Panel/Background/Slot1/Sapphire.show()
+		$CanvasLayer/Panel/Background/Sapphire.hide()
 		slot_1_full = true
 	elif slot_2_full == false:
-		$CanvasLayer/Panel/Background/Slots/Slot2/Sapphire.show()
-		$CanvasLayer/Panel/Background/Gems/Sapphire.hide()
+		$CanvasLayer/Panel/Background/Slot2/Sapphire.show()
+		$CanvasLayer/Panel/Background/Sapphire.hide()
 		slot_2_full = true
 	elif slot_3_full == false:
-		$CanvasLayer/Panel/Background/Slots/Slot3/Sapphire.show()
-		$CanvasLayer/Panel/Background/Gems/Sapphire.hide()
+		$CanvasLayer/Panel/Background/Slot3/Sapphire.show()
+		$CanvasLayer/Panel/Background/Sapphire.hide()
 		slot_3_full = true
 
 ### RUBY ###
 
 func _on_ruby_button_mouse_entered():
-	$CanvasLayer/Panel/Background/Gems/Ruby/selector4.show()
+	$CanvasLayer/Panel/Background/Ruby/selector4.show()
 
 
 func _on_ruby_button_mouse_exited():
-	$CanvasLayer/Panel/Background/Gems/Ruby/selector4.hide()
+	$CanvasLayer/Panel/Background/Ruby/selector4.hide()
 
 
 func _on_ruby_button_pressed():
 	if all_slots_full:
 		return
 	if slot_1_full == false:
-		$CanvasLayer/Panel/Background/Slots/Slot1/Ruby.show()
-		$CanvasLayer/Panel/Background/Gems/Ruby.hide()
+		$CanvasLayer/Panel/Background/Slot1/Ruby.show()
+		$CanvasLayer/Panel/Background/Ruby.hide()
 		slot_1_full = true
 	elif slot_2_full == false:
-		$CanvasLayer/Panel/Background/Slots/Slot2/Ruby.show()
-		$CanvasLayer/Panel/Background/Gems/Ruby.hide()
+		$CanvasLayer/Panel/Background/Slot2/Ruby.show()
+		$CanvasLayer/Panel/Background/Ruby.hide()
 		slot_2_full = true
 	elif slot_3_full == false:
-		$CanvasLayer/Panel/Background/Slots/Slot3/Ruby.show()
-		$CanvasLayer/Panel/Background/Gems/Ruby.hide()
+		$CanvasLayer/Panel/Background/Slot3/Ruby.show()
+		$CanvasLayer/Panel/Background/Ruby.hide()
 		slot_3_full = true
 
 ### AMETHYST ###
 
 func _on_amethyst_button_mouse_entered():
-	$CanvasLayer/Panel/Background/Gems/Amethyst/selector5.show()
+	$CanvasLayer/Panel/Background/Amethyst/selector5.show()
 
 
 func _on_amethyst_button_mouse_exited():
-	$CanvasLayer/Panel/Background/Gems/Amethyst/selector5.hide()
+	$CanvasLayer/Panel/Background/Amethyst/selector5.hide()
 
 
 func _on_amethyst_button_pressed():
 	if all_slots_full:
 		return
 	if slot_1_full == false:
-		$CanvasLayer/Panel/Background/Slots/Slot1/Amethyst.show()
-		$CanvasLayer/Panel/Background/Gems/Amethyst.hide()
+		$CanvasLayer/Panel/Background/Slot1/Amethyst.show()
+		$CanvasLayer/Panel/Background/Amethyst.hide()
 		slot_1_full = true
 	elif slot_2_full == false:
-		$CanvasLayer/Panel/Background/Slots/Slot2/Amethyst.show()
-		$CanvasLayer/Panel/Background/Gems/Amethyst.hide()
+		$CanvasLayer/Panel/Background/Slot2/Amethyst.show()
+		$CanvasLayer/Panel/Background/Amethyst.hide()
 		slot_2_full = true
 	elif slot_3_full == false:
-		$CanvasLayer/Panel/Background/Slots/Slot3/Amethyst.show()
-		$CanvasLayer/Panel/Background/Gems/Amethyst.hide()
+		$CanvasLayer/Panel/Background/Slot3/Amethyst.show()
+		$CanvasLayer/Panel/Background/Amethyst.hide()
 		slot_3_full = true
 
 
