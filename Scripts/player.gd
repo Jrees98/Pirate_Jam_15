3a69a22fa4934ad37f647bdf4405a03ba1049e81
Jacob Rees <reescodes@gmail.com>
2024-07-27 15:09:31 -0600

    animations work now

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 var speed : int
 var in_minecart : bool
+var is_mining = false
 func _ready():
 	$RockNodeUI/CoalSprite/CoalQuantity.text = str(Global.total_coal)
 	$RockNodeUI/HBoxContainer/BlueGemSprite/GemQuantity.text = str(Global.total_gems)
@@ -12,41 +13,50 @@ func _ready():
 	$RockNodeUI/SanityBar.value = 100
 	
 func _physics_process(delta):
+	speed = Global.move_speed
+	update_ui()
+	if not in_minecart:
+		move_player()
+		check_mining()
+	else:
+		move_minecart()
+	check_minecart()
+	
+func move_player():
 	var direction = Input.get_axis("move_left","move_right")
-	if in_minecart == false:
-		if direction:
+	if not is_mining:
+		if direction != 0:
 			velocity.x = speed * direction
 			$AnimatedSprite2D.play("walk")
-
+			$AnimatedSprite2D.flip_h = direction < 0
 		else:
 			velocity.x = 0
 			$AnimatedSprite2D.play("default")
 		move_and_slide()
-		
-		if direction > 0:
-			$AnimatedSprite2D.play("walk")
-			$AnimatedSprite2D.flip_h = false
-		if direction < 0:
-			$AnimatedSprite2D.flip_h = true
-	if in_minecart:
-		if direction:
-			velocity.x = speed * direction
-		else:
-			velocity.x = 0
-		move_and_slide()
-		
-		if direction > 0:
-			$AnimatedSprite2D.flip_h = false
-		if direction < 0:
-			$AnimatedSprite2D.flip_h = true
-	if Input.is_action_pressed("minecart") and Global.purchased_minecart == true:
+
+func check_mining():
+	if Input.is_action_just_pressed("interact"):
+		is_mining = true
+		$AnimatedSprite2D.play("mining")
+	if Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_right"):
+		is_mining = false
+
+func check_minecart():
+	if Input.is_action_just_pressed("minecart") and Global.purchased_minecart == true:
 		$AnimatedSprite2D.play("minecart")
 		in_minecart = true
-		Global.move_speed = 3000
-	
-	
 
-	
+
+func move_minecart():
+	var direction = Input.get_axis("move_left","move_right")
+	if direction != 0:
+		velocity.x = speed * direction
+		$AnimatedSprite2D.flip_h = direction < 0
+	else:
+		velocity.x = 0
+	move_and_slide()
+
+func update_ui():
 	#update UI counts
 	$RockNodeUI/CoalSprite/CoalQuantity.text = str(Global.total_coal)
 	$RockNodeUI/HBoxContainer/BlueGemSprite/GemQuantity.text = str(Global.total_gems)
@@ -56,12 +66,8 @@ func _physics_process(delta):
 	$RockNodeUI/HBoxContainer/PurpleGemSprite4/GemQuantity.text = str(Global.total_purplegem)
 	$RockNodeUI/HBoxContainer/WhiteGemSprite5/GemQuantity.text = str(Global.total_whitegem)
 	$RockNodeUI/HBoxContainer/OpalGemSprite6/OpalGemQuantity.text = str(Global.total_opals)
-	speed = Global.move_speed
 	
-	
-
 func _on_regain_energy_timeout():
-
 	$RockNodeUI/SanityBar.value += 10
 
 
